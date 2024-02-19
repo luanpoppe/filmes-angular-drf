3ca60c76c26605db5580443db29a14ee1a85b665
luanpoppe <luanpoppe@gmail.com>
2024-02-19 15:21:28 -0300

    feat: teste do Navbar concluído

--- front-end/src/app/navbar/navbar.component.spec.ts
+++ front-end/src/app/navbar/navbar.component.spec.ts
@@ -2,9 +2,10 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavbarComponent } from './navbar.component';
 
-describe('NavbarComponent', () => {
+describe('Layout do NavbarComponent', () => {
   let component: NavbarComponent;
   let fixture: ComponentFixture<NavbarComponent>;
+  let navbar: any
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -15,11 +16,27 @@ describe('NavbarComponent', () => {
     fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    navbar = fixture.nativeElement as HTMLElement
   });
 
   it('deve ter o nome da empresa', () => {
-    const navbar = fixture.nativeElement as HTMLElement
-    const nomeEmpresa = navbar.querySelector(".navbar-brand h1")
+    const nomeEmpresa = navbar.querySelector(".navbar-brand h1") as HTMLElement
     expect(nomeEmpresa?.textContent).toBe("Filmes - Solos");
   });
+
+  it("imagem correta deve estar no src da <img>", () => {
+    const img = navbar.querySelector(".navbar-brand img") as HTMLImageElement
+    expect(img.src).toBe("https://solosbrasil.com/wp-content/uploads/2021/05/logo-do-site.svg")
+  })
+
+  it("deve ter 2 submenus", () => {
+    const div = navbar.querySelectorAll("#navbarSupportedContent ul li") as HTMLElement[]
+    expect(div.length).toBe(2)
+  })
+
+  it("deve ter os nomes dos submenus corretos", () => {
+    const div = navbar.querySelectorAll("#navbarSupportedContent ul li") as HTMLElement[]
+    expect(div[0].textContent).toBe("Home")
+    expect(div[1].textContent).toBe("Login")
+  })
 });
